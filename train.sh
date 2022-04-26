d72d90aba5e2ffc5a013f6a5658e1d572641bed1
linkserendipity <462710683@qq.com>
2022-04-26 12:38:33 +0800

    train.sh

--- train.sh
+++ train.sh
@@ -1,1 +1,2 @@
-CUDA_VISIBLE_DEVICES=1 python -m torch.distributed.launch --nproc_per_node=4 --master_port 66666 train.py --config_file configs/VehicleID/vit_base.yml MODEL.DIST_TRAIN True
+# CUDA_VISIBLE_DEVICES=1 python -m torch.distributed.launch --nproc_per_node=4 --master_port 66666 train.py --config_file configs/VehicleID/vit_base.yml MODEL.DIST_TRAIN True
+python train.py --config_file configs/DukeMTMC/vit_base.yml MODEL.DEVICE_ID "('0')"
